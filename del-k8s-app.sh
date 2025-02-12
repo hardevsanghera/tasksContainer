dd47b94b2dd1f4f40d6cf71bdaa972ec56f35b15
Hardev Sanghera <hardev_sanghera@yahoo.co.uk>
2025-02-12 15:51:30 +0000

    finalizing

---
+++ del-k8s-app.sh
@@ -0,0 +1,7 @@
+kubectl delete deploy prodtasksproj
+kubectl delete deploy nginx
+kubectl delete service prodtasksproj
+kubectl delete service nginx
+kubectl delete pvc static-data
+kubectl get all
+kubectl get pvc
---
+++ start-k8s-app.sh
@@ -0,0 +1,7 @@
+kubectl -f prodtasksproj-service.yaml apply
+kubectl -f nginx-service.yaml apply
+kubectl -f static-data-persistentvolumeclaim.yaml apply
+kubectl -f prodtasksproj-deployment.yaml apply
+kubectl -f nginx-deployment.yaml apply
+kubectl get all
+kubectl get pvc
