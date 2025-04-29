06c1d7fefb0775d4519467a22c3093795ff45916
hardevsanghera <hardev_sanghera@yahoo.co.uk>
2025-04-29 16:52:13 +0100

    Delete del-k8s-app.sh

--- del-k8s-app.sh
+++
@@ -1,7 +0,0 @@
-kubectl delete deploy prodtasksproj
-kubectl delete deploy nginx
-kubectl delete service prodtasksproj
-kubectl delete service nginx
-kubectl delete pvc static-data
-kubectl get all
-kubectl get pvc
