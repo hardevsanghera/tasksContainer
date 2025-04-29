1674ecb1e49cdc546dc7cca2163bd804bc11644b
hardevsanghera <hardev_sanghera@yahoo.co.uk>
2025-04-29 11:36:44 +0100

    Update start-k8s-app.sh

--- start-k8s-app.sh
+++ start-k8s-app.sh
@@ -1,8 +1,10 @@
+# Order is important
+kubectl -f prodtasksproj-deployment.yaml apply
 kubectl -f prodtasksproj-service.yaml apply
 kubectl -f nginx-service.yaml apply
 kubectl -f static-pv.yaml apply
 kubectl -f static-data-persistentvolumeclaim.yaml apply
-kubectl -f prodtasksproj-deployment.yaml apply
+#kubectl -f prodtasksproj-deployment.yaml apply
 kubectl -f nginx-deployment.yaml apply
 kubectl get all
 kubectl get pvc
